4e5c79ee542f7ebd1230bca0cdd71a41224564fe
Evan McLean <evanhughmclean@gmail.com>
2022-06-01 13:47:10 -0400

    Adds Game Screen Module
    Created a module for when the game screen is active. This adds a Game
    board object as well as functions for rendering X's and O's on the board
    and for determining who's turn it is.

--- scripts.js
+++ scripts.js
@@ -1,24 +1,75 @@
-/* const settingsScreen = (function() {
-
-})();
- */
-const gameScreen = (function() {
-    
-})();
-
 const Player = function (sign, type) {
     let score = 0;
     const getPlayerType = () => {
-        type;
+        return type;
     };
     const getSign = () => {
-        sign;
+        return sign;
     };
     const incrementScore = () => {
         score += 1;
     };
     const getScore = () => {
-        score;
+        return score;
     }
     return {getSign, getPlayerType, incrementScore, getScore};
 };
+
+/* const settingsScreen = (function() {
+
+})();
+ */
+const gameScreen = (function() {
+    const player1 = Player("X", "Human");
+    const player2 = Player("O", "Human");
+    let player1Turn = true;
+    const player1Moves = [];
+    const player2Moves = [];
+    const tile = document.querySelectorAll(".tile");
+
+    const renderGameBoard = () => {
+        let i = 0;
+        const gameboard = gameBoard.getGameBoard();
+        gameboard.forEach(element => {
+            document.querySelector(`[data-index="${i}"]`).textContent = element;
+            i++;
+        });
+    };
+
+    const gameBoard = (function () {
+        const board = ["", "", "", "", "", "", "", "", ""];
+        const setValue = (index, sign) => {
+            board[index] = sign;
+        };
+        const getGameBoard = () => {
+            return board;
+        };
+        return {getGameBoard, setValue};
+    })();
+
+    const game = (function () {
+        const board = gameBoard.getGameBoard();
+        const playRound = (e) => {
+            let index = e.target.dataset.index;
+            if (board[index] != "") {
+                return;
+            }
+            else {
+                if (player1Turn) {
+                    player1Moves.push(index);
+                    gameBoard.setValue(index, player1.getSign());
+                    player1Turn = false;
+                }
+                else {
+                    player2Moves.push(index);
+                    gameBoard.setValue(index, player2.getSign());
+                    player1Turn = true;
+                }
+            }
+            renderGameBoard();
+        };
+        return {playRound};
+    })();
+
+    tile.forEach(square => {square.addEventListener("click", game.playRound)});
+})();
